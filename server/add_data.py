123c23f86a4cbd0e721e68a1b096abd0eea1054d
ReeceAdamsStone <reeceadamsstone@gmail.com>
2024-01-19 13:53:25 +0000

    added animal farm strings and added AF to data script

--- server/add_data.py
+++ server/add_data.py
@@ -3,50 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from models import *
 from app import app
 from typing import List
+from question_strings import *
 
-
-LP1Q5Strings = [
-    "Either: Write a description of a train station as suggested by this picture Or: Write the opening of a story that begins ‘When he left that day, I did not know I would not see him again for five years… '",
-
-    "Either: Write a description of a harbour as suggested by this scene Or: Write the opening of a story that begins ‘I decided it was time to go out on my boat for the first time since the accident…'",
-
-    "Either: Write a description of a street fruit-seller as suggested by this scene Or: Write the opening of a story that begins ‘Despite the woman’s poverty, she taught me so much that morning about life…'",
-
-    "Either: Write a description of a countryside setting as suggested by this picture Or: Write the opening of a story that begins ‘After living here for three months, I realised I was not cut out for life in the country after all…'",
-
-    "Either: Write a description of a hospital setting as suggested by this picture Or: Write the opening of a story that begins ‘The doctor’s first day on the job at the new hospital was certainly going to be challenging…'",
-
-    "Either: Write a description of a fairground as suggested by this picture Or: Write the opening of a story that begins ‘The noise and bustle of the crowd did not distract the detective from the task to find the fairground killer…'",
-
-    "Either: Write a description of a trek through the snow as suggested by this picture Or: Write the opening of a story that begins ‘As dawn broke across the snow-filled landscape, we faced the most gruelling trek of our lives…’",
-
-    "Either: Write a description of a hot air balloon flight as suggested by this picture Or: Write the opening of a story that begins ‘As we ascended into the sky that warm, summer evening, I did not consider the danger…'",
-
-    "Either: Write a description of a dark street as suggested by this picture OR Write a story about someone feeling unsure or challenged.",
-
-    "Either: Write a description of a sunny day, based on this picture Or: Write the opening of a story about an adventure that starts in this place.",
-      
-    "Either: Write a story set during a festival as suggested by this picture. OR: Write a story about a celebration going wrong",
-
-    "Either: Write the opening of a story based on this picture. OR: Write the opening part of a story when a character has just suffered defeat or loss",
-
-    "Either: Write the opening of a story suggested by this picture. OR: Write the opening part of a story when a character is put in an unexpected situation.",
-
-    "Either: Write a story in which a photograph plays a significant part. Or: Write a description suggested by this photograph:",
-]
-
-def add_assessment_objective(ao_name: str) -> int:
-    # Check if the Assessment Objective already exists
-    existing_ao = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Assessment Objective {ao_name}').first()
-
-    if not existing_ao:
-        # If it doesn't exist, add it to the database
-        new_ao = Assessment_Objectives(AO_Int_Name=f'Assessment Objective {ao_name}')
-        db.session.add(new_ao)
-        db.session.flush()
-        return new_ao.AO_ID
-
-    return existing_ao.AO_ID
 
 def get_paper_component_topic_ids(session, paper_name, component_name, topic_name, marks):
     # Check if the Paper_Name already exists
@@ -105,7 +63,6 @@ def get_paper_component_topic_ids(session, paper_name, component_name, topic_nam
     return paper_id, component_id, topic_id
 
 
-
 with app.app_context():
     for question_string in LP1Q5Strings:
         lp1q5_instance = LP1Q5(question_string)
@@ -121,14 +78,20 @@ with app.app_context():
 
         db.session.add(Questions(Question_String=lp1q5_instance.question_string, Topic_ID=1, Supporting_Material=lp1q5_instance.supporting_material))
 
-        # Only add AO relationships if the AO_ID exists
-        result_ao_1 = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Objective {lp1q5_instance.ao_id[0]}').first()
-        if result_ao_1:
-            db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=result_ao_1.AO_ID))
 
-        result_ao_2 = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Objective {lp1q5_instance.ao_id[1]}').first()
-        if result_ao_2:
-            db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=result_ao_2.AO_ID))
+    for af_string in AFStrings:
+        lit_animal_farm_instance = LitAnimalFarm(af_string)
+
+        # Use the function to get paper_id, component_id, and topic_id for LitAnimalFarm
+        af_paper_id, af_component_id, af_topic_id = get_paper_component_topic_ids(
+            db.session,
+            lit_animal_farm_instance.paper_name,
+            lit_animal_farm_instance.paper_component,
+            lit_animal_farm_instance.topic,
+            lit_animal_farm_instance.marks
+        )
+
+        db.session.add(Questions(Question_String=lit_animal_farm_instance.question_string, Topic_ID=2, Supporting_Material=lit_animal_farm_instance.supporting_material))
 
     db.session.commit()
 
@@ -174,10 +137,38 @@ with app.app_context():
     
 
 
-
         # # Add Assessment Objectives
         # ao_id_1 = add_assessment_objective(lp1q5_instance.ao_id[0])
         # db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=ao_id_1))
 
         # ao_id_2 = add_assessment_objective(lp1q5_instance.ao_id[1])
         # db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=ao_id_2))
+
+        # db.session.add(Questions(Question_String=lp1q5_instance.question_string, Topic_ID=1, Supporting_Material=lp1q5_instance.supporting_material))
+
+        # Only add AO relationships if the AO_ID exists
+
+        # result_ao_1 = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Objective {lp1q5_instance.ao_id[0]}').first()
+        # if result_ao_1:
+        #     db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=result_ao_1.AO_ID))
+
+        # result_ao_2 = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Objective {lp1q5_instance.ao_id[1]}').first()
+        # if result_ao_2:
+        #     db.session.add(AOs_By_Paper(QID=db.session.query(Questions).filter_by(Question_String=lp1q5_instance.question_string).first().QiD, AO_ID=result_ao_2.AO_ID))
+    
+
+
+
+    
+# def add_assessment_objective(ao_name: str) -> int:
+#     # Check if the Assessment Objective already exists
+#     existing_ao = db.session.query(Assessment_Objectives).filter_by(AO_Int_Name=f'Assessment Objective {ao_name}').first()
+
+#     if not existing_ao:
+#         # If it doesn't exist, add it to the database
+#         new_ao = Assessment_Objectives(AO_Int_Name=f'Assessment Objective {ao_name}')
+#         db.session.add(new_ao)
+#         db.session.flush()
+#         return new_ao.AO_ID
+
+#     return existing_ao.AO_ID
---
+++ server/question_strings.py
@@ -0,0 +1,51 @@
+
+LP1Q5Strings = [
+    "Either: Write a description of a train station as suggested by this picture Or: Write the opening of a story that begins ‘When he left that day, I did not know I would not see him again for five years… '",
+
+    "Either: Write a description of a harbour as suggested by this scene Or: Write the opening of a story that begins ‘I decided it was time to go out on my boat for the first time since the accident…'",
+
+    "Either: Write a description of a street fruit-seller as suggested by this scene Or: Write the opening of a story that begins ‘Despite the woman’s poverty, she taught me so much that morning about life…'",
+
+    "Either: Write a description of a countryside setting as suggested by this picture Or: Write the opening of a story that begins ‘After living here for three months, I realised I was not cut out for life in the country after all…'",
+
+    "Either: Write a description of a hospital setting as suggested by this picture Or: Write the opening of a story that begins ‘The doctor’s first day on the job at the new hospital was certainly going to be challenging…'",
+
+    "Either: Write a description of a fairground as suggested by this picture Or: Write the opening of a story that begins ‘The noise and bustle of the crowd did not distract the detective from the task to find the fairground killer…'",
+
+    "Either: Write a description of a trek through the snow as suggested by this picture Or: Write the opening of a story that begins ‘As dawn broke across the snow-filled landscape, we faced the most gruelling trek of our lives…’",
+
+    "Either: Write a description of a hot air balloon flight as suggested by this picture Or: Write the opening of a story that begins ‘As we ascended into the sky that warm, summer evening, I did not consider the danger…'",
+
+    "Either: Write a description of a dark street as suggested by this picture OR Write a story about someone feeling unsure or challenged.",
+
+    "Either: Write a description of a sunny day, based on this picture Or: Write the opening of a story about an adventure that starts in this place.",
+      
+    "Either: Write a story set during a festival as suggested by this picture. OR: Write a story about a celebration going wrong",
+
+    "Either: Write the opening of a story based on this picture. OR: Write the opening part of a story when a character has just suffered defeat or loss",
+
+    "Either: Write the opening of a story suggested by this picture. OR: Write the opening part of a story when a character is put in an unexpected situation.",
+
+    "Either: Write a story in which a photograph plays a significant part. Or: Write a description suggested by this photograph:",
+]
+
+
+AFStrings = [
+    "How does Orwell explore ideas about rebellion in Animal Farm? Write about: • how Orwell presents the ideas about rebellion in Animal Farm • how Orwell presents these ideas about rebellion in his manipulation of language.",
+    "How does Orwell use the character of Snowball to explore ideas about learning and progress? Write about: • how Orwell presents the character of Snowball • how Orwell uses the character of Snowball to present learning and progress.",
+    "How does Orwell use the character of Boxer in Animal Farm to explore ideas about loyalty? Write about: • How Orwell presents the character of Boxer and loyalty • How Orwell uses Boxer to present ideas about society in the novel.",
+    "‘All animals are equal, but some are more equal than others.’ How far is this idea important in Animal Farm? Write about: • what you think Orwell is saying about equality and inequality • how Orwell presents these ideas through the events of the novel.",
+    "In Animal Farm, Boxer says ‘I must work harder’. How does Orwell explore attitudes towards work in Animal Farm? Write about: • how Orwell presents some of the attitudes towards work • how Orwell uses these attitudes to explore ideas about society.",
+    "Who do you think is the most powerful character in Animal Farm? Write about: • what your chosen character says and does and how they are powerful • how Orwell presents your chosen character.",
+    "What do you think is the importance of the ending of Animal Farm? Write about: • how the ending of the novel presents some important ideas • how Orwell presents these ideas by the ways he writes.",
+    "'Squealer is the most influential character in Animal Farm.’ Explore how far you agree with this statement. Write about: • how Orwell presents Squealer and others’ attitudes towards him • how Orwell uses the character of Squealer to explore some of his ideas about influence and manipulation.",
+    "'Society is the real victim of Animal Farm.’ Explore how far you agree with this statement. Write about: • how Orwell presents society • how Orwell uses the farm animals to explore some of his ideas.",
+    "At the start of Animal Farm, Old Major says: ‘Only get rid of man, and the produce of labour would be our own.’ How does Orwell present ideas about freedom in Animal Farm? Write about: • what some of the ideas about being a freedom are • how Orwell presents these ideas by the ways he writes.",
+    "In Animal Farm, Orwell describes Napoleon as the: ‘terror of mankind, protector of the sheepfolk.’ How does Orwell present Napoleon as a leader? Write about: • how Orwell presents the character of Napoleon • how Orwell uses the character of Napoleon to explore ideas about leadership.",
+    "What do you think is the importance of the windmill in Animal Farm? Write about: • how the windmill presents some important ideas • how Orwell presents these ideas by the way he writes.",
+    "How does Orwell present ideas about hope in Animal Farm? Write about: • what some of the ideas about being a hope are • how Orwell presents these ideas by the ways he writes.",
+    "How does Orwell use the character of Napoleon to explore ideas about power and control in Animal Farm? Write about: • how Orwell presents the character of Napoleon • how Orwell uses the character of Napoleon to present ideas about power and control in the novel.",
+    "‘Because they didn’t take the time to invest in their education like the pigs, the other animals only have themselves to blame for their demise.’ To what extent do you agree? Write about: • Characters’ attitudes towards education • The difference in the education of the pigs compared to the other animals.",
+    "‘You can’t have power without corruption’ To what extent do you agree? Write about: • How Orwell presents those in power in the novel • How Orwell presents power and corruption through the way he writes.",
+    "How does Orwell use the principles of Animalism to warn his readers about the society at the time? Write about: • How Animalism is presented in the novel • How the corruption of Animalism is presented in the novel."
+]
