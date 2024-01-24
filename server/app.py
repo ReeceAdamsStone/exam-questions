efb813f11988489c3652f6ec6574aa47303a72d3
ReeceAdamsStone <reeceadamsstone@gmail.com>
2024-01-24 18:45:27 +0000

    created api routing for front end

--- server/app.py
+++ server/app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.middleware.proxy_fix import ProxyFix
 from sqlalchemy.orm import DeclarativeBase
-# from models import db
+import routes
+from flask_cors import CORS
 
 app =Flask(__name__)
+CORS(app) 
 
 class Base(DeclarativeBase):
   pass
@@ -23,9 +25,42 @@ def return_home():
         'message': "Hello World!"
     })
 
+
+
+
+
 db = SQLAlchemy(model_class=Base)    
 
-import models
+from models import *
+
+@app.route('/api/papers')
+def get_paper_names():
+    papers = Paper_Name.query.all()
+    # paper_names = [paper.Paper_Name for paper in papers]
+    paper_names = [{ 'paper_id': paper.Paper_ID, 'paper_name': paper.Paper_Name} for paper in papers]
+    return jsonify(paper_names)
+
+
+@app.route('/api/questions', methods=['GET'])
+def get_questions():
+    questions = Questions.query.all()
+    question_data = [{'id': question.QiD, 'question_string': question.Question_String} for question in questions]
+    return jsonify(question_data)
+
+
+@app.route('/api/topics', methods=['GET'])
+def get_topics():
+    topics = Topics.query.all()
+    topic_data = [{'id': topic.Topic_ID, 'topic_name': topic.Topic_Name, 'component_id': topic.Component_of_Paper_ID} for topic in topics]
+    return jsonify(topic_data)
+
+
+@app.route('/api/component', methods=['GET'])
+def get_components():
+    components = Component_of_Paper.query.all()
+    component_data = [{'component_id': component.Component_of_Paper_ID, 'component_name': component.Component_Name, 'marks': component.Marks, 'paper_id': component.Paper_ID} for component in components]
+    return jsonify(component_data)
+
 
 db.init_app(app)
 
